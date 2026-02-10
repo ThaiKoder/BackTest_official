527f9849a91b28dea7a391bd3c751a31271b40a7
ThaiKoder <thaikoder@gmail.com>
2026-02-10 15:34:22 +0100

    Struct Candle M1

---
+++ DatasetTool/JsonToBinaryConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DatasetTool
+{
+    public readonly record struct Candle1m(long TsNs, long O, long H, long L, long C, long V);
+
+ 
+}
