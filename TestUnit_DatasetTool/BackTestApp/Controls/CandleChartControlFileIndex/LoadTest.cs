46d908423c901e70695d577171f88af59c949949
ThaiKoder <thaikoder@gmail.com>
2026-03-07 04:50:33 +0100

    Cas de teste à mettre en place

--- TestUnit_DatasetTool/BackTestApp/Controls/CandleChartControlFileIndex/LoadTest.cs
+++ TestUnit_DatasetTool/BackTestApp/Controls/CandleChartControlFileIndex/LoadTest.cs
@@ -34,4 +34,32 @@ public class LoadTest
         }
     }
 
+    //Test3 : Parcourir liste fichiers index et qu'ils sont dans l'ordre croissant
+
+    //Test4 : Parcourir liste fichiers index et vérifier pas de doublons
+
+    //Test5 : Parcourir liste fichiers index et vérifier x premiers et x derniers records en fonction de l'index courant
+
+    //Test6 : Verifier x premiers et x derniers records en fonction de l'index courant 0 et len -1
+
+    //Test7 : Parcourirs liste fichiers avec recherche binaire et verifier l'idx + nom fichier + x derniers et x premiers en fonction de la position idx
+
+    //Test8 : Modifier dynamiquement x dernier et x premiers en fonction de l'idx courant
+
+    //Parcourir tout les fichiers avec Next File
+
+
+
+    //Test3 : charger un fichier index invalide (ex: non existant)
+
+    //Test4 : charger un fichier index avec un format incorrect (ex: pas de records)
+
+    //Test5 : charger un fichier index avec des records partiels (ex: taille du fichier pas multiple de 8)
+
+    //Test6 : charger un fichier index valide et vérifier le count
+
+    //Test7 : charger un fichier index valide et vérifier la lecture d'un record (ex: lire le premier record et vérifier sa valeur)
+
+    //
+
 }
